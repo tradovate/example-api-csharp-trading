e76066ed7f3d69bac5212371f9ac5a9ee3d15c84
Victor Vins <vvins@tradovate.com>
2021-05-13 19:37:34 -0400

    cid and sec added to AccessTokenRequest

--- Tradovate.Samples/Authentication.cs
+++ Tradovate.Samples/Authentication.cs
@@ -17,7 +17,7 @@ namespace Tradovate
         public static AccessTokenResponse GetAccessToken(string basePath, string username, string password)
         {
             var apiInstance = new AuthenticationApi(basePath);
-            var body = new AccessTokenRequest(name: username, password: password, appId: "SampleApp", appVersion: "0.1.0");
+            var body = new AccessTokenRequest(name: username, password: password, appId: "SampleApp", appVersion: "0.1.0", cid: "8", sec: "f03741b6-f634-48d6-9308-c8fb871150c2");
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
             AccessTokenResponse result = apiInstance.AccessTokenRequest(body);
             Debug.WriteLine(result);
